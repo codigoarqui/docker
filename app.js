25751289768a5146c5691b36916c3b646ee62160
codigoarqui <codigoarqui.examples@gmail.com>
2025-11-26 18:35:19 -0500

    docker capas

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('¡Hola! Esta es mi primera imagen propia en Docker 🐳')
+})
+
+app.listen(port, () => {
+  console.log(`La aplicación está escuchando en el puerto ${port}`)
+})
